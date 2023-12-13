2ed77deabd47e08a325015b0d9d40c882b445217
Jenny <147312222+jennywhitmore@users.noreply.github.com>
2023-12-13 16:56:59 +0100

    fix: code issues code now switches third and second checked checkbox

--- checkboxes/script.js
+++ checkboxes/script.js
@@ -1,21 +1,23 @@
-const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+let checkedCheckboxes = [];
 
-function handleCheckboxChange(event) {
-  let checkedCount = 0;
-  let lastCheckedIndex = null;
-
-  checkboxes.forEach((checkbox, index) => {
-    if (checkbox.checked) {
-      checkedCount++;
-      lastCheckedIndex = index;
-    }
-  });
-
-  if (checkedCount > 2) {
-    checkboxes[lastCheckedIndex].checked = false;
-  }
-}
-
-checkboxes.forEach((checkbox) => {
+document.querySelectorAll("input[type=checkbox]").forEach(function (checkbox) {
   checkbox.addEventListener("change", handleCheckboxChange);
 });
+
+function handleCheckboxChange(event) {
+  if (event.target.checked) {
+    if (checkedCheckboxes.length === 2) {
+      // Uncheck the second checkbox that was checked
+      checkedCheckboxes[1].checked = false;
+      // Remove the second checkbox from the array
+      checkedCheckboxes.splice(1, 1);
+    }
+    // Add the newly checked checkbox to the array
+    checkedCheckboxes.push(event.target);
+  } else {
+    // Remove the unchecked checkbox from the array
+    checkedCheckboxes = checkedCheckboxes.filter(
+      (checkbox) => checkbox !== event.target
+    );
+  }
+}
