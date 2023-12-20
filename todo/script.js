699b57f95caa799b9f258afac09113322bdb3b0a
Jenny <147312222+jennywhitmore@users.noreply.github.com>
2023-12-20 21:51:38 +0100

    initial commit

--- todo/script.js
+++ todo/script.js
@@ -1,26 +1,161 @@
-const state = { todos: [{ item: "Apfel" }, { item: "Banane" }] };
-renderToDos();
-function renderToDos() {
-  const list = document.querySelector("#list");
-  list.innerHTML = "";
-  state.todos.forEach((el) => {
-    const newLi = document.createElement("li");
-    const text = document.createTextNode(el.item);
-    newLi.appendChild(text);
-    list.appendChild(newLi);
-  });
-}
-const form = document.querySelector("form");
-form.addEventListener("submit", addItem);
-function addItem(event) {
-  event.preventDefault(); // This will stop the default form submission behavior.
+////////////////////////////////////////////////////
+// Variable declarations
+////////////////////////////////////////////////////
+const addButton = document.querySelector("#add");
+const clearButton = document.querySelector("#clear-done");
+const radioButtons = document.querySelectorAll(".radio");
+const todoList = document.querySelector("#todo-list");
+const todoInput = document.querySelector("#todo-input");
 
-  const userInputField = document.querySelector("#userInput");
-  if (userInputField.value.trim() !== "") {
-    // Check if the input is not empty
-    const newItem = { item: userInputField.value.trim() };
-    state.todos.push(newItem);
-    renderToDos(); // Render the updated list of todos.
-    userInputField.value = ""; // Clear the input field after adding the item.
+////////////////////////////////////////////////////
+// Event listeners
+////////////////////////////////////////////////////
+addButton.addEventListener("click", function () {
+  handleAddButtonClick();
+});
+clearButton.addEventListener("click", function () {
+  handleClearButtonClick();
+});
+
+window.addEventListener("load", function () {
+  loadState();
+  attachEventListeners();
+});
+
+radioButtons.forEach((radio) => {
+  radio.addEventListener("click", function (event) {
+    filterToDos(event);
+    saveState();
+  });
+});
+
+todoInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    handleAddButtonClick();
+  }
+});
+
+////////////////////////////////////////////////////
+// Function definitions
+////////////////////////////////////////////////////
+
+function createTodoItem(text, checked) {
+  const listItem = document.createElement("li");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = checked;
+  listItem.appendChild(checkbox);
+
+  const todoID = generateUniqueID();
+  listItem.id = todoID;
+
+  const label = document.createElement("label");
+  label.textContent = text;
+  listItem.appendChild(label);
+
+  listItem.addEventListener("click", function () {
+    listItem.classList.toggle("checked");
+  });
+  return listItem;
+}
+
+function generateUniqueID() {
+  return "todo-" + Math.floor(Math.random() * 1000);
+}
+
+function handleAddButtonClick() {
+  const todoText = todoInput.value.trim();
+  if (todoText !== "") {
+    const todoItem = createTodoItem(todoText, false);
+    todoList.appendChild(todoItem);
+    saveState();
+    todoInput.value = "";
+  }
+}
+
+function handleClearButtonClick() {
+  const items = todoList.querySelectorAll("li");
+  for (const item of items) {
+    if (item.classList.contains("checked")) {
+      todoList.removeChild(item);
+    }
+  }
+  saveState();
+}
+
+function showAllItems() {
+  const items = todoList.querySelectorAll("li");
+  for (const item of items) {
+    item.style.display = "block";
+  }
+}
+
+function showUncheckedItems() {
+  const items = todoList.querySelectorAll("li");
+  for (const item of items) {
+    if (!item.classList.contains("checked")) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  }
+}
+
+function showCheckedItems() {
+  const items = todoList.querySelectorAll("li");
+  for (const item of items) {
+    if (item.classList.contains("checked")) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  }
+}
+
+function filterToDos(event) {
+  const selectedRadio = event.target.id;
+  switch (selectedRadio) {
+    case `all`:
+      showAllItems();
+      break;
+    case `still`:
+      showUncheckedItems();
+      break;
+    case `done`:
+      showCheckedItems();
+      break;
+    default:
+      showAllItems();
+  }
+}
+
+function saveState() {
+  const allTodos = [];
+  const items = todoList.querySelectorAll("li");
+  for (const item of items) {
+    const todo = {
+      id: item.id,
+      text: item.querySelector("label").textContent,
+      checked: item.classList.contains("checked"),
+    };
+    allTodos.push(todo);
+  }
+  localStorage.setItem("todos", JSON.stringify(allTodos));
+}
+
+function loadState() {
+  const savedTodos = localStorage.getItem("todos");
+  if (savedTodos) {
+    const allTodos = JSON.parse(savedTodos);
+    // Remove checked items before loading
+    todoList.innerHTML = "";
+    for (const todo of allTodos) {
+      if (!todo.checked) {
+        const todoItem = createTodoItem(todo.text, todo.checked);
+        todoItem.id = todo.id;
+        todoList.appendChild(todoItem);
+      }
+    }
   }
 }
